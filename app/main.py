4f9125449ec8777be1a94fd1fce8452b0b1e9ab1
Yisus2299 <jmzm08@gmail.com>
2026-07-26 01:16:53 -0400

    fix: update import statements to use relative paths

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.core.config import settings
-from app.api.v1 import users
+from .core.config import settings
+from .api.v1 import users
 
 app = FastAPI(
     title=settings.APP_NAME,
